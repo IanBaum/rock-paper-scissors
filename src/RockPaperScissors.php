fbe4936b5e3619b69ab022d74921d926fb60772d
Ian Baum and Rebecca Allen <rebecca@>
2016-09-14 15:28:50 -0400

    add lobster images for all winning conditions

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -1,28 +1,43 @@
 <?php
     class RockPaperScissors{
 
+      public $winningAttack;
+
       function playGame($first_input, $second_input){
         if($first_input === $second_input){
           return "Draw";
         }
+
         if($first_input === "rock" && $second_input === "scissors"){
+          $this->winningAttack = "rock";
           return "Player 1";
         }
         if($first_input === "rock" && $second_input === "paper"){
+          $this->winningAttack = "paper";
           return "Player 2";
         }
         if($first_input === "paper" && $second_input === "rock"){
+          $this->winningAttack = "paper";
           return "Player 1";
         }
         if($first_input === "paper" && $second_input === "scissors"){
+          $this->winningAttack = "scissors";
           return "Player 2";
         }
         if($first_input === "scissors" && $second_input === "paper"){
+          $this->winningAttack = "scissors";
           return "Player 1";
         }
         if($first_input === "scissors" && $second_input === "rock"){
+          $this->winningAttack = "rock";
           return "Player 2";
         }
       }
+
+      // function checkWinner(){
+      //   if($this->winningAttack === "rock"){
+      //     return "rock"
+      //   }
+      // }
     }
 ?>
